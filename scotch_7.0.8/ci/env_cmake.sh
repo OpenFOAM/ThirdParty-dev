6609cd2c41764e02382c210a50b0751d82bc3600
Henry Weller <http://cfd.direct>
2025-10-31 14:00:41 +0000

    Scotch: Upgraded from version 7.0.8 to 7.0.10

--- scotch_7.0.8/ci/env_cmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-case "${CC}" in
-     icx )
-       source /disk1/builds/intel/setvars.sh intel64 && export MPI_HOME=/disk1/builds/intel/mpi/latest/ ;;
-     pgcc )
-       source /disk1/builds/nvidia/hpc_sdk/load_env ;;
-esac
